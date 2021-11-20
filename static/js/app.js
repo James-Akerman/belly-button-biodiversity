64bacea97bc21b98702599a7143e02a3977d95d0
unknown <james.l.akerman@gmail.com>
2021-11-21 10:39:47 +1100

    third draft

--- static/js/app.js
+++ static/js/app.js
@@ -1,71 +1,47 @@
-d3.json("samples.json").then((importedData) => {
-  
-  data = importedData;
-
-  // Step 1: Seperate the data into different arrays of metadata, names, and sample data
-  const metadata = [];
-  const names = [];
-  const samples = [];
-  sample_length = data['metadata'].length;  // All the arrays are of the same length: 153
-  for (let i = 0; i < sample_length; i++) {
-     metadata.push(data['metadata'][i]);
-     names.push(data['names'][i]);
-     samples.push(data['samples'][i]);
+// The option changed function
+function optionChanged(){
+  let dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  let dataset = dropdownMenu.property("value");
+  // When the value in the select box changes, then change the horizontal bar chart
+  filteredData = samples.filter(entry => entry.id === dataset)[0];
+  // CREATE THE HORIZONTAL BAR GRAPH
+  // Select top 10 sample values
+  let top_ten_sample_values = filteredData.sample_values.slice(0, 10).reverse();
+  let top_ten_sample_otu_ids = filteredData.otu_ids.map(entry => 'OTU ' + entry).slice(0, 10).reverse();
+  let top_ten_sample_otu_labels = filteredData.otu_labels.slice(0, 10).reverse();
+  // Create a trace object
+  let trace1 = {
+    x: top_ten_sample_values,
+    y: top_ten_sample_otu_ids,
+    text: top_ten_sample_otu_labels,
+    type: "bar",
+    orientation: "h"
+  };
+  // Create a data array with the above trace
+  let plot_data = [trace1];
+  // Use `layout` to define a title
+  let layout = {
+    title: "Basic horizontal barplot",
+  };
+  Plotly.newPlot("bar", plot_data, layout);
   };
 
-  // Step 2: divide each object in each array
-      // Divide the metadata data
 
-      // Divide the name data
-
-      // Divide the sample data
-      sample_otu_ids = samples.map(entry => entry.otu_ids);
-      console.log(sample_otu_ids);
-
-
-
+d3.json("samples.json").then((importedData) => {
+  data = [importedData];
+  // Seperate the data into different arrays of metadata, names, and sample data
+  metadata = data.map(entry => entry.metadata)[0];
+  names = data.map(entry => entry.names)[0];
+  samples = data.map(entry => entry.samples)[0];
+  // Assign each test subject id to the select dropdown menu
+  var sample_ids = samples.map(entry => entry.id);
+  var selector = d3.select("#selDataset");
+  sample_ids.forEach((sample) => {
+    selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+  });
+  optionChanged();
 });
-
-
-  //2.Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-  // -Use sample_values as the values for the bar chart.
-  // -Use otu_ids as the labels for the bar chart.
-  // -Use otu_labels as the hovertext for the chart.
-
-
-  // Get the sample ids, sample values, otu_ids, and otu_labels
-//   var sample_ids = [];
-//   var sample_values = [];
-//   var otu_ids = [];
-//   var otu_labels = [];
-
-//   for (let i = 0; i < sample_amount; i++) {
-//     sample_ids.push(samples[i]['id']);
-//     otu_ids.push(samples[i]['otu_ids']);
-//     otu_labels.push(samples[i]['otu_labels']);
-//     sample_values.push(samples[i]['sample_values']);
-//   };
-
-//   console.log(otu_ids);
-
-// // Create a trace object
-// var trace1 = {
-//   y: otu_ids,
-//   x: sample_values,
-//   type: "bar",
-//   oriantation: 'h'
-// };
-
-
-// // Create a data array with the above trace
-// var data = [trace1];
-
-// // Use `layout` to define a title
-// var layout = {
-//   title: "Basic horizontal barplot"
-// };
-
-// // Render the plot to the `plot1` div
-// Plotly.newPlot("bar", data, layout);
-
-  // });
