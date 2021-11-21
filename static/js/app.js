f0ad27f9a748d6f79a6730ff73280ea7b8045449
unknown <james.l.akerman@gmail.com>
2021-11-21 18:15:00 +1100

    added gauge chart first draft

--- static/js/app.js
+++ static/js/app.js
@@ -40,6 +40,7 @@ function optionChanged(){
       x: filteredSampleData.otu_ids,
       y: filteredSampleData.sample_values,
       mode: 'markers',
+      text: filteredSampleData.otu_labels,
       marker: {
         size: filteredSampleData.sample_values,
         color: filteredSampleData.otu_ids,
@@ -55,7 +56,123 @@ function optionChanged(){
   };
   Plotly.newPlot('bubble', bubble_data, bubble_layout);
 
+  // GAUGE CHART
+    // let gauge_data = [{
+    // type: "pie",
+    // showlegend: false,
+    // hole: 0.4,
+    // rotation: 90,
+    // values: [40,5,5,5,5,5,5,5,5],
+    // labels: ["","0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "7-8", "8-9"],
+    // direction: "clockwise",
+    // marker: {
+    //   colors: [
+    //         'rgb(255, 255, 255)',
+    //         'rgb(232,226,202)',
+    //         'rgb(226,210,172)',
+    //         'rgb(223,189,139)',
+    //         'rgb(223,162,103)',
+    //         'rgb(226,126,64)',
+    //         'rgb(226,126,64)',
+    //         'rgb(226,126,64)',
+    //         'rgb(226,126,64)'
+    //   ]
+    // },
+    // hoverinfo: 'skip',
+    // textposition: 'none',
+    // }];
 
+    // let gauge_layout = {
+    //   title: "Belly Button Washing Frequency".bold() + "<br>" + 'Scrubs per Week',
+    // };
+
+    // let gauge_data = [
+    //   {
+    //     domain: { x: [0, 1], y: [0, 1] },
+    //     // value: 2,
+    //     title: { text: "Belly Button Washing Frequency".bold() + "<br>" + 'Scrubs per Week' },
+    //     type: "indicator",
+    //     mode: "gauge",
+    //     gauge: {
+    //       axis: { range: [0, 9], showticklabels: false },
+    //       steps: [
+    //         {names: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "7-8", "8-9"]},
+    //         { range: [0, 1], color: "blue"},
+    //         { range: [1, 2], color: "red" },
+    //         { range: [2, 3], color: "green" },
+    //         { range: [3, 4], color: "yellow" },
+    //         { range: [4, 5], color: "orange" },
+    //         { range: [5, 6], color: "pink" },
+    //         { range: [6, 7], color: "lightblue" },
+    //         { range: [7, 8], color: "purple" },
+    //         { range: [8, 9], color: "lightgreen" },
+    //       ],
+    //       threshold: {
+    //         line: { color: "red", width: 4 },
+    //         thickness: 0.75,
+    //         value: 490
+    //       },
+    //       labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "7-8", "8-9"],
+    //     }
+    //   }
+    // ];
+    
+    // let gauge_layout = { 
+    //   width: 600, 
+    //   height: 450, 
+    //   margin: { t: 0, b: 0 } };
+
+    gauge_data = [{
+      values: [50, 10, 10, 10, 10, 10],
+      labels: [" ", "Debug", "Info", "Warn", "Error", "Fatal"],
+      marker: {
+          'colors': [
+              'rgb(255, 255, 255)',
+              'rgb(232,226,202)',
+              'rgb(226,210,172)',
+              'rgb(223,189,139)',
+              'rgb(223,162,103)',
+              'rgb(226,126,64)'
+          ]
+      },
+      domain: {"x": [0, 0.48]},
+      name: "Gauge",
+      hole: .3,
+      type: "pie",
+      direction: "clockwise",
+      rotation: 90,
+      showlegend: false,
+      textinfo: "label",
+      textposition: "inside",
+      hoverinfo: "none"
+  }]
+
+  gauge_layout = {
+    title: { 
+      text: "Belly Button Washing Frequency".bold() + "<br>" + 'Scrubs per Week', 
+      x: 0.1
+    },
+    shapes: [
+        {
+          // Debug
+          path: 'M 0.235 0.5 L 0.14 .60 L 0.245 0.5 Z',
+          // Info
+          // path: 'M 0.235 0.5 L 0.19 0.65 L 0.245 0.5 Z',
+          // Warn
+          // path: 'M 0.235 0.5 L 0.24 0.72 L 0.245 0.5 Z',
+          // Error
+          // path: 'M 0.235 0.5 L 0.29 0.65 L 0.245 0.5 Z',
+          // Fatal
+          // path: 'M 0.235 0.5 L 0.35 0.57 L 0.245 0.5 Z',
+
+          fillcolor: 'red',
+          line: {
+            width: 0.4
+          },
+        }
+    ],
+    }
+  Plotly.newPlot('gauge', gauge_data, gauge_layout);
 
 };
 
